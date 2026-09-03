9f0aeb749f9c03567095d9476797e0b667c7eed6
M Umar Adnan <umaradnan2710@gmail.com>
2026-09-03 11:10:01 +0500

    Class without a constructor but an initializer function

--- Object & Class.py
+++ Object & Class.py
@@ -1,8 +1,15 @@
 class Student:
-    name = "John"  #class variable
-    age = 20       #class variable
+    name = ""  #class variable
+    age = None       #class variable
+    def initialize(n, a):  #constructor
+        name = n  #instance variable
+        age = a    #instance variable
+
 
 s1=Student() # This line creates an instance of the Student class with the name "John" and age 20
+s1.name=input("Enter the name of the student: ")  #Assigning value to the instance variable 'name' of the object 's1'
+s1.age=int(input("Enter the age of the student: "))  #Assigning value to the instance variable 'age' of the object 's1'
+
 
 #Printing the values of the instance variables 'name' and 'age' of the object 's1'
 print(s1.name)
