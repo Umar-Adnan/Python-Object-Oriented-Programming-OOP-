c1ee9445a09500eb8c486afd8d907c97a247d29b
M Umar Adnan <umaradnan2710@gmail.com>
2026-09-03 10:45:35 +0500

    Object and Classes

---
+++ Object & Class.py
@@ -0,0 +1,9 @@
+class Student:
+    name = "John"  #class variable
+    age = 20       #class variable
+
+s1=Student() # This line creates an instance of the Student class with the name "John" and age 20
+
+#Printing the values of the instance variables 'name' and 'age' of the object 's1'
+print(s1.name)
+print(s1.age)
