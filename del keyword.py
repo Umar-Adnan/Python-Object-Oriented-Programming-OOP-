474f32715cfd7e6c859f6c9ab3d24d487711bac5
M Umar Adnan <umaradnan2710@gmail.com>
2026-09-04 18:29:48 +0500

    Used to delete attributes, mehtods of an object or an object itself

--- del keyword.py
+++ del keyword.py
@@ -6,8 +6,8 @@ class Student:
     def display_info(self):
         print(f"Name: {self.name}, Age: {self.age}")
 
-    s1 = Student("Alice", 20)
-    del s1.age
-    s1.display_info()  # This will raise an AttributeError since 'age' has been deleted.
-    del s1  # This will delete the instance 's1' of the Student class.
-    s1.display_info()  # This will raise a NameError since 's1' has been deleted.
+s1 = Student("Alice", 20)
+del s1.age
+s1.display_info()  # This will raise an AttributeError since 'age' has been deleted.
+del s1  # This will delete the instance 's1' of the Student class.
+s1.display_info()  # This will raise a NameError since 's1' has been deleted.
